eb298df4f116833daf3254660d6d07bf438a2916
dtcortese <dcortese@chicagobooth.edu>
2021-11-29 05:25:06 +0000

    Through Users Tab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,21 @@ class UsersController < ApplicationController
     matching_users = User.all 
     
     @list_of_users = matching_users.order({ :username => :asc})
-    
+
     render({ :template => "user_templates/index.html.erb"})
+  end
+
+  def show
+  
+    # Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_username = User.where ({:username => url_username })
+
+    @the_user = matching_username.at(0)
+
+    render({ :template => "user_templates/show.html.erb"})
+  
   end
 
 
